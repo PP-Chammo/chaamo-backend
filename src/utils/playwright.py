a8722c25af531189913cbb380aae891ce291c9b5
PP-Chammo <pasquale.palena.chammo@gmail.com>
2025-07-29 16:27:52 +0100

    chore: set headless to False

--- src/utils/playwright.py
+++ src/utils/playwright.py
@@ -6,7 +6,7 @@ from playwright.async_api import async_playwright
 import os
 import sys
 
-headless = True
+headless = False
 
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
